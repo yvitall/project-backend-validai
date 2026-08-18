4628fe3e64637b44ba69cd0b3c8136efbeae3082
yvitall <yuritsvital05@gmail.com>
2026-08-18 13:47:31 -0300

    feat: core spring boot architecture initialization

---
+++ src/main/java/com/yvital/validai/ValidaiApplication.java
@@ -0,0 +1,13 @@
+package com.yvital.validai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yvital/validai/ValidaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yvital.validai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
