501f226ed36626834e3e7ea234474bbfdb9c6b12
yvitall <yuritsvital05@gmail.com>
2026-09-16 20:06:07 -0300

    feat(domain): create User entity and UserRole enum

---
+++ src/main/java/com/yvital/validai/domain/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.yvital.validai.domain.enums;
+
+public enum UserRole {
+    ADMIN,
+    ORGANIZER,
+    PARTICIPANT
+}
---
+++ src/main/java/com/yvital/validai/domain/model/User.java
@@ -0,0 +1,53 @@
+package com.yvital.validai.domain.model;
+
+import java.time.LocalDateTime;
+
+import com.yvital.validai.domain.enums.UserRole;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Email
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 20)
+    private UserRole role = UserRole.PARTICIPANT;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
